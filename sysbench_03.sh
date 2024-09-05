c3bfff637e453aa115816bb4af50b2abf5e75bd3
santosg572 <santosg572@gmail.com>
2024-09-05 09:10:07 -0600

    Create sysbench_03.sh

---
+++ sysbench_03.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Estresando el disco
+
+sysbench --test=fileio --file-total-size=3G prepare
+sysbench --test=fileio --file-total-size=3G --file-test-mode=rndrw --max-time=300 --max-requests=0 run
+sysbench --test=fileio --file-total-size=3G cleanup
